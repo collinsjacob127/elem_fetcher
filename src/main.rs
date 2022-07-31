93d975e665438d23902b63264edd097c4f106a2c
Jacob Collins <collinsjacob127@gmail.com>
2022-07-31 14:21:18 -0700

    First Working Prototype!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-
+/*
+Jacob Collins
+Rust Web Scraper
+July 31, 2022
+*/
+use scraper::{Html, Selector};
 fn main() {
     // Getting full info of web page with reqwest
     let response = reqwest::blocking::get(
@@ -8,12 +13,33 @@ fn main() {
     .text()
     .unwrap();
     // Using scraper to parse the response
-    let document = scraper::Html::parse_document(&response);
+    let document = Html::parse_document(&response);
     // Defining the query to sort through document with
-    let title_selector = scraper::Selector::parse("td.a").unwrap();
+    let title_selector = Selector::parse("td>a").unwrap();
     // Finding what we want in document 
     let titles = document.select(&title_selector).map(|x| x.inner_html());
+    // Running through each found element and printing them
     titles
         .zip(1..20)
         .for_each(|(item, number)| println!("{}. {}", number, item));
+    println!("Task Finished");
 }
+// fn main() {
+//     let response = reqwest::blocking::get(
+//         "https://www.imdb.com/search/title/?groups=top_100&sort=user_rating,desc&count=100",
+//     )
+//     .unwrap()
+//     .text()
+//     .unwrap();
+
+//     let document = Html::parse_document(&response);
+
+//     let title_selector = Selector::parse("h3.lister-item-header>a").unwrap();
+
+//     let titles = document.select(&title_selector).map(|x| x.inner_html());
+
+//     titles
+//         .zip(1..101)
+//         .for_each(|(item, number)| println!("{}. {}", number, item));
+//     println!("Task Finished");
+// }
