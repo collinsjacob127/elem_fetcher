48cf54c232cc5299b55ec63695c90fbd3357fe5b
collinsjacob127 <collinsjacob127@gmail.com>
2022-08-01 12:36:18 -0700

    testing tag requirements for reqwest inputs

--- examples/main.rs
+++ examples/main.rs
@@ -6,8 +6,8 @@ July 31, 2022
 use web_scraper_rs::scavenger::scavenger;
 fn main() {
     let test_output = 
-        scavenger("https://returnofthemounthuasectmanhwa.com",
-        ["td>a", "td>i"].to_vec());
+        scavenger("https://www.asurascans.com/comics/1649969363-return-of-the-mount-hua-sect/",
+        ["div.infox>h1.entry-title", "a>span.chapternum", "a>span.chapterdate"].to_vec());
     match test_output {
         Ok(test_output) => {
             let output_str = [test_output[0][0].to_string(), 
